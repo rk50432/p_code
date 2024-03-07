93fdfe858a2e39586cc8f096e48b3466d12970be
jenny.zeng <jenny.zeng@pilotgaea.com.tw>
2024-03-07 15:00:23 +0800

    add sort.py

---
+++ sort.py
@@ -0,0 +1,7 @@
+def bubblesort(arr):
+    for i in range(1,len(arr)):
+        for j in range(0,len(arr)-i):
+            if arr[j]>arr[j+1]:
+                arr[j],arr[j+1]=arr[j+1],arr[j]
+    return arr
+print(bubblesort([9,6,3,17,5]))
