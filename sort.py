eb39b008cde5f9ddf8612ac8cc57d308cac90dcb
jenny.zeng <jenny.zeng@pilotgaea.com.tw>
2024-03-08 10:51:03 +0800

    update sort.py

--- sort.py
+++ sort.py
@@ -5,3 +5,21 @@ def bubblesort(arr):
                 arr[j],arr[j+1]=arr[j+1],arr[j]
     return arr
 print(bubblesort([9,6,3,17,5]))
+
+from timeit import default_timer as timer
+
+# list
+start = timer()
+for x in range(100):
+    j=[]
+    for i in range(10000): j.append(i**2)
+end =timer()
+print(end-start)
+
+# list comprehensions
+start = timer()
+for x in range(100):
+    j=[i**2 for i in  range(10000)]
+end =timer()
+print (end -start)
+
