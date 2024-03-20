cec5659f38c1cd91d84577931df453d2d5d3cc6e
jenny.zeng <jenny.zeng@pilotgaea.com.tw>
2024-03-20 15:08:22 +0800

    update base.py

--- base.py
+++ base.py
@@ -1,4 +1,24 @@
-x=5
-y="jas"
-print(type(x))
-print(type(y))
+import pymssql
+import pyodbc
+
+
+def conn():
+
+    connect = pyodbc.connect(
+                'DRIVER={{ODBC Driver 18 for SQL Server}};'
+                'SERVER=P1A-11127-0\SQLEXPRESS;'
+                'USERNAME=sa;'
+                'PASSWORD=12345;'
+                'DATABASE="TncDig";'
+                'Trusted_Connection=yes;'
+    )
+    if connect:
+
+        print("連線成功")
+
+    else:
+
+        print("連線失敗")
+
+    return connect
+
